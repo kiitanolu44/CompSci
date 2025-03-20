c4443a1cdf6b51e8061c283ee1552fd0574f1a86
Kiitan Oluwasere <kiitanoluwasere@Kiitans-MacBook-Pro.local>
2025-03-20 19:54:53 +0000

    refactoring

--- butterfly_effect.py
+++ butterfly_effect.py
@@ -1,3 +1,37 @@
+def butterfly_effect(
+      xs: list[float], 
+      ys: list[float], 
+      zs: list[float], 
+      iterations: int
+) -> tuple[list[float], list[float], list[float]]:
+   for i in range(iterations):
+      x = xs[i-1]
+      y = ys[i-1]
+      z = zs[i-1]
+
+      new_x = y - x
+      new_x *= σ
+      new_x *= dt
+      new_x += x
+
+      new_y = ρ - z
+      new_y *= x
+      new_y -= y
+      new_y *= dt
+      new_y += y
+
+      new_z = x * y
+      new_z -= β * z
+      new_z *= dt
+      new_z += z
+
+      xs.append(new_x)
+      ys.append(new_y)
+      zs.append(new_z)
+
+   return xs, ys, zs
+
+
 σ=10.
 ρ=28.
 β=8./3.
@@ -11,62 +45,8 @@ xs2 = [-12.2]
 ys2 = [-13.2]
 zs2 = [31.2]
 
-i = 0
-while i < 1000:
-   i += 1
-
-   x = xs[i-1]
-   y = ys[i-1]
-   z = zs[i-1]
-
-   new_x = y - x
-   new_x *= σ
-   new_x *= dt
-   new_x += x
-
-   new_y = ρ - z
-   new_y *= x
-   new_y -= y
-   new_y *= dt
-   new_y += y
-
-   new_z = x * y
-   new_z -= β * z
-   new_z *= dt
-   new_z += z
-
-   xs.append(new_x)
-   ys.append(new_y)
-   zs.append(new_z)     
-
-i = 0
-while i < 1000:
-   i += 1
-
-   x = xs2[i-1]
-   y = ys2[i-1]
-   z = zs2[i-1]
-
-   new_x = y - x
-   new_x *= σ
-   new_x *= dt
-   new_x += x
-
-   new_y = ρ - z
-   new_y *= x
-   new_y -= y
-   new_y *= dt
-   new_y += y
-
-   new_z = x * y
-   new_z -= β * z
-   new_z *= dt
-   new_z += z
-
-   xs2.append(new_x)
-   ys2.append(new_y)
-   zs2.append(new_z)
-
+xs, ys, zs = butterfly_effect(xs=xs, ys=ys, zs=zs, iterations=1000)
+xs2, ys2, zs2 = butterfly_effect(xs=xs2, ys=ys2, zs=zs2, iterations=1000)
 
 print(xs[-1],ys[-1],zs[-1])
 print(xs2[-1],ys2[-1],zs2[-1])
